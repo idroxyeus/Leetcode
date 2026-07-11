0578b09868d0f4425b8d4bc9c29ceeee4b929dc8
Dharanidharan <dharanidharan514@gmail.com>
2026-07-11 20:33:09 +0530

    Time: 191 ms (25.17%) | Memory: 20.7 MB (41.43%) - LeetSync

---
+++ 907-koko-eating-bananas/koko-eating-bananas.py
@@ -0,0 +1,12 @@
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        l, r = 1, max(piles)
+
+        while l < r:
+            m = (l + r) // 2
+            if sum((p + m - 1) // m for p in piles) <= h:
+                r = m
+            else:
+                l = m + 1
+
+        return l
